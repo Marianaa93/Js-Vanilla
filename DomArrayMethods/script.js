7839d502f7e7f111755dce419e96bb3402582b41
Mariana Maia <marianalima.amb@gmail.com>
2022-12-29 17:37:55 -0300

    for each methodo and display

--- DomArrayMethods/script.js
+++ DomArrayMethods/script.js
@@ -1,1 +1,42 @@
-script.js;
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMilionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+const btnApi = document.getElementById("fetchApi");
+
+let data = [];
+
+async function getRandomUser() {
+  const res = await fetch("http://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last} `,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+  console.log(newUser);
+}
+
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+function updateDOM(providedData = data) {
+  main.innerHTML = "<h2><strong>Person</strong>Wealth</h2>";
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong>${item.money}`;
+    main.appendChild(element);
+  });
+}
+
+addUserBtn.addEventListener("click", getRandomUser);
